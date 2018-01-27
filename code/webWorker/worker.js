6eacd566c693efe95845bf784887fee6dca1f71f
RonkTsang <1037568810@qq.com>
2018-01-27 17:36:20 +0800

    html,performance

---
+++ code/webWorker/worker.js
@@ -0,0 +1,10 @@
+onmessage = function (e) {
+  console.log('Message received from main script');
+  var workerResult = 'Result: ' + (e.data[0] * e.data[1]);
+  console.log('Posting message back to main script');
+  postMessage(workerResult);
+}
+
+console.log(self)
+
+// console.log('window:', window) undefined
